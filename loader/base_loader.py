909f5d3edce5907f44f30fb670ae7be443674347
DailyDreaming <lblauvel@ucsc.edu>
2018-05-24 11:58:45 -0700

    Doctstrings.

--- loader/base_loader.py
+++ loader/base_loader.py
@@ -44,6 +44,16 @@ CREATOR_ID = 20
 
 class DssUploader:
     def __init__(self, dss_endpoint: str, staging_bucket: str, google_project_id: str, dry_run: bool) -> None:
+        """
+        Functions for uploading files to a given DSS.
+
+        :param dss_endpoint: The URL to a Swagger DSS API.  e.g. "https://commons-dss.ucsc-cgp-dev.org/v1"
+        :param staging_bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param google_project_id: A google "Project ID", link is:
+                                  https://console.cloud.google.com/cloud-resource-manager
+                                  e.g. "platform-dev-178517"
+        :param dry_run: True or False
+        """
         self.dss_endpoint = dss_endpoint
         self.staging_bucket = staging_bucket
         self.google_project_id = google_project_id
@@ -56,6 +66,17 @@ class DssUploader:
         self.dss_client.host = self.dss_endpoint
 
     def upload_cloud_file(self, bucket: str, key: str, bundle_uuid: str, file_uuid: str) -> tuple:
+        """
+        Upload a file contained in an s3 bucket into a given DSS.
+
+        NOTE: S3 ONLY!
+
+        :param bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :param file_uuid: A uuid.uuid4() format hash.
+        :return: file_uuid: str, file_version: str, filename: str
+        """
         if not self._has_hca_tags(self.blobstore, bucket, key):
             checksums = self._calculate_checksums(self.s3_client, bucket, key)
             self._set_hca_metadata_tags(self.s3_client, bucket, key, checksums)
@@ -67,13 +88,33 @@ class DssUploader:
                                        file_cloud_urls: set,
                                        bundle_uuid: str,
                                        guid: str) -> tuple:
+        """
+        Takes a file's metadata, formats it as a dictionary, and uploads this dictionary as a json file to the DSS.
+
+        :param filename: The basename of the file in the bucket.
+        :param file_uuid: A uuid.uuid4() format hash.
+        :param file_cloud_urls: A set of 'gs://' and 's3://' bucket links.
+                                e.g. {'gs://broad-public-datasets/g.bam', 's3://ucsc-topmed-datasets/a.bam'}
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :param guid: A uuid.uuid4() format hash.
+        :return: file_uuid: str, file_version: str, filename: str
+        """
         if self.dry_run:
             logger.info(f"DRY RUN: upload_cloud_file_by_reference: {filename} {str(file_cloud_urls)} {bundle_uuid}")
+
         file_reference = self._create_file_reference(file_cloud_urls, guid)
         return self.upload_dict_as_file(file_reference, filename, file_uuid, bundle_uuid,
                                         "application/json; dss-type=fileref")
 
     def _create_file_reference(self, file_cloud_urls: set, guid: str) -> dict:
+        """
+        Format a file's metadata into a dictionary for uploading as a json.
+
+        :param file_cloud_urls: A set of 'gs://' and 's3://' bucket links.
+                                e.g. {'gs://broad-public-datasets/g.bam', 's3://ucsc-topmed-datasets/a.bam'}
+        :param guid: A uuid.uuid4() format hash.
+        :return: A dictionary of metadata values.
+        """
         s3_metadata = None
         gs_metadata = None
         for cloud_url in file_cloud_urls:
@@ -89,6 +130,13 @@ class DssUploader:
         return self._consolidate_metadata(file_cloud_urls, s3_metadata, gs_metadata, guid)
 
     def _get_s3_file_metadata(self, bucket: str, key: str) -> dict:
+        """
+        Format an s3 file's metadata into a dictionary for uploading as a json.
+
+        :param bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :return: A dictionary of metadata values.
+        """
         metadata = dict()
         try:
             response = self.s3_client.head_object(Bucket=bucket, Key=key, RequestPayer="requester")
@@ -100,6 +148,13 @@ class DssUploader:
         return metadata
 
     def _get_gs_file_metadata(self, bucket: str, key: str) -> dict:
+        """
+        Format a gs file's metadata into a dictionary for uploading as a json.
+
+        :param bucket: Base name of a google bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: gs file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :return: A dictionary of metadata values.
+        """
         metadata = dict()
         try:
             gs_bucket = self.gs_client.bucket(bucket, self.google_project_id)
@@ -112,7 +167,21 @@ class DssUploader:
         return metadata
 
     @staticmethod
-    def _consolidate_metadata(file_cloud_urls: set, s3_metadata: Optional[Dict[str, Any]], gs_metadata: Optional[Dict[str, Any]], guid: str) -> dict:
+    def _consolidate_metadata(file_cloud_urls: set,
+                              s3_metadata: Optional[Dict[str, Any]],
+                              gs_metadata: Optional[Dict[str, Any]],
+                              guid: str) -> dict:
+        """
+        Creates a dictionary concordant with the swagger json specification in
+        preparation to uploading to the DSS.
+
+        :param file_cloud_urls: A set of 'gs://' and 's3://' bucket links.
+                                e.g. {'gs://broad-public-datasets/g.bam', 's3://ucsc-topmed-datasets/a.bam'}
+        :param s3_metadata: Dictionary of meta data produced by _get_s3_file_metadata().
+        :param gs_metadata: Dictionary of meta data produced by _get_gs_file_metadata().
+        :param guid: A uuid.uuid4() format hash.
+        :return: A dictionary of metadata values.
+        """
         consolidated_metadata = dict()
         if s3_metadata:
             consolidated_metadata.update(s3_metadata)
@@ -124,6 +193,16 @@ class DssUploader:
         return consolidated_metadata
 
     def upload_dict_as_file(self, value: dict, filename: str, file_uuid: str, bundle_uuid: str, content_type=None):
+        """
+        Upload a dictionary representing a file's metadata as a json to the DSS.
+
+        :param value: A dictionary of metadata values.
+        :param filename: The basename of the file in the bucket.
+        :param file_uuid: A uuid.uuid4() format hash.
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :param content_type: Content description, for example: "application/json; dss-type=fileref".
+        :return: file_uuid: str, file_version: str, filename: str
+        """
         tempdir = mkdtemp()
         file_path = "/".join([tempdir, filename])
         with open(file_path, "w") as fh:
@@ -135,10 +214,25 @@ class DssUploader:
 
     # TODO Add ability to specify file_uuid
     def upload_local_file(self, path: str, file_uuid: str, bundle_uuid: str, content_type=None):
+        """
+
+        :param path: Path to a local file.
+        :param file_uuid: A uuid.uuid4() format hash.
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :param content_type: Content description, for example: "application/json; dss-type=fileref".
+        :return: file_uuid: str, file_version: str, filename: str
+        """
         file_uuid, key = self._upload_local_file_to_staging(path, file_uuid, content_type)
         return self._upload_tagged_cloud_file_to_dss(self.staging_bucket, key, file_uuid, bundle_uuid)
 
     def load_bundle(self, file_info_list: list, bundle_uuid: str):
+        """
+        Loads a bundle to the DSS.
+
+        :param file_info_list:
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :return: A string representing: "{bundle_uuid}.{version}"
+        """
         kwargs = dict(replica="aws", creator_uid=CREATOR_ID, files=file_info_list, uuid=bundle_uuid)
         if not self.dry_run:
             response = self.dss_client.put_bundle(**kwargs)
@@ -152,22 +246,51 @@ class DssUploader:
 
     @staticmethod
     def get_filename_from_key(key: str):
+        assert not key.endswith('/'), 'Please specify a filename, not a directory ({} cannot end in "/").'.format(key)
         return key.split("/")[-1]
 
     # TODO Add ability to specify file_uuid
     def _upload_local_file_to_staging(self, path: str, file_uuid: str, content_type):
+        """
+        Uploads a local file to an s3 bucket.
+
+        :param path: Path to a local file.
+        :param file_uuid: A uuid.uuid4() format hash.
+        :param content_type: Content description, for example: "application/json; dss-type=fileref".
+        :return: file_uuid: str, key_name: str
+        """
         with open(path, "rb") as fh:
             file_uuid, key_name = upload_to_cloud(fh, file_uuid, self.staging_bucket, content_type)
         return file_uuid, key_name
 
     @staticmethod
     def _has_hca_tags(blobstore: BlobStore, bucket: str, key: str) -> bool:
+        """
+        Return True if any of the following tags are found:
+            "hca-dss-s3_etag"
+            "hca-dss-sha1"
+            "hca-dss-sha256"
+            "hca-dss-crc32c"
+
+        :param blobstore: cloud.blobstore.BlobStore
+        :param bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :return: bool
+        """
         hca_tag_names = {"hca-dss-s3_etag", "hca-dss-sha1", "hca-dss-sha256", "hca-dss-crc32c"}
         metadata = blobstore.get_user_metadata(bucket, key)
         return hca_tag_names.issubset(metadata.keys())
 
     @staticmethod
     def _calculate_checksums(s3_client, bucket: str, key: str) -> typing.Dict:
+        """
+        Creates checksums for a file in an s3 bucket.
+
+        :param s3_client: An instance of boto3.client("s3").
+        :param bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :return: A dictionary of checksums.
+        """
         checksumming_sink = ChecksummingSink()
         tx_cfg = TransferConfig(multipart_threshold=S3Etag.etag_stride,
                                 multipart_chunksize=S3Etag.etag_stride)
@@ -175,6 +298,14 @@ class DssUploader:
         return checksumming_sink.get_checksums()
 
     def _set_hca_metadata_tags(self, s3_client, bucket: str, key: str, checksums: dict) -> None:
+        """
+        Sets checksums for each hca metadata tag for a file.
+
+        :param s3_client: An instance of boto3.client("s3").
+        :param bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :param checksums: A dictionary of checksums.
+        """
         metadata = {
             "hca-dss-s3_etag": checksums["s3_etag"],
             "hca-dss-sha1": checksums["sha1"],
@@ -188,6 +319,16 @@ class DssUploader:
 
     def _upload_tagged_cloud_file_to_dss(self, source_bucket: str, source_key: str, file_uuid: str, bundle_uuid: str,
                                          timeout_seconds=1200):
+        """
+        Uploads a tagged file contained in a cloud bucket to the DSS.
+
+        :param source_bucket: Base name of an s3 bucket.  e.g. 'mbaumann-dss-staging'
+        :param source_key: s3 file to upload.  e.g. 'output.txt' or 'data/output.txt'
+        :param file_uuid: A uuid.uuid4() format hash.
+        :param bundle_uuid: A uuid.uuid4() format hash.
+        :param timeout_seconds:  Amount of time to continue attempting an async copy.
+        :return: file_uuid: str, file_version: str, filename: str
+        """
         source_url = f"s3://{source_bucket}/{source_key}"
         filename = self.get_filename_from_key(source_key)
 
@@ -254,8 +395,8 @@ class MetadataFileUploader:
         return self.dss_uploader.upload_dict_as_file(metadata, filename, str(uuid.uuid4()), bundle_uuid)
 
 def load_json_from_file(input_file_path: str) -> dict:
-        with open(input_file_path) as fh:
-            return json.load(fh)
+    with open(input_file_path) as fh:
+        return json.load(fh)
 
 def suppress_verbose_logging():
     for logger_name in logging.Logger.manager.loggerDict:  # type: ignore
